690ab35e238f3600a1a9aca6798d4566789938f4
Edwin659 <gwennaelle.airo-farulla@isen.yncrea.fr>
2025-01-07 15:09:41 +0100

    Add files via upload

---
+++ Echecs/C/fourni.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h> // pour time()
+
+//////////////////////////////////////////
+// Constantes:
+
+// Option pour tourner l’échiquier lorsque le joueur noir est en train de jouer:
+#define TOURNER_ECHIQUIER false
+
+// Contenu d’une case vide de l’échiquier:
+#define CASE_VIDE ' '
+
+// L’échiquier sera modélisé par un tableau 2D de caractères, de taille NxN.
+#define N 8
+
+// Nombre maximal de pièces d'une couleur pouvant être capturées.
+#define NB_MAX_CAPTURES 15
+
+// Nombre maximal de coup possible pour une pièce. Ne pas modifier:
+#define NB_MAX_DEPL (4*(N)-5)
+
+// Encodage des couleurs d'une case de l'échiquier:
+#define C_BLANC 0
+#define C_NOIR  1
+#define C_VIDE  2
+
+// Encodage de l'état en cours d'une partie:
+#define NON_TERMINEE  0
+#define DEFAITE_BLANC 1
+#define DEFAITE_NOIR  2
+#define PARTIE_NULLE  3
+
+// Encodage des pièces par un caractère - majuscules pour
+// le joueur blanc et minuscules pour le joueur noir:
+// Tour:     'T' ou 't'
+// Fou:      'F' ou 'f'
+// Dame:     'D' ou 'd'
+// Roi:      'R' ou 'r'
+// Cavalier: 'C' ou 'c'
+// Pion:     'P' ou 'p'
+
+//////////////////////////////////////////
+// Prototypes des fonctions fournies:
+
+// Ne pas modifier les prototypes ci-dessous.
+// Si l'on souhaite implémenter des fonctionnalités non exigées, on pourra
+// faire cela en ajoutant des variables globales et de nouvelles fonctions.
+
+void effaceConsole(void);
+void afficheCaseEnCouleur(char c, int couleurEnEchec);
+
+//////////////////////////////////////////
+// Prototypes des fonctions à implémenter:
+
+// Initialisation et affichage:
+bool estDansGrille(int ligne, int colonne);
+bool estCaseVide(const char grille[N][N], int ligne, int colonne);
+void videGrille(char grille[N][N]);
+void copieGrille(char copie[N][N], const char grille[N][N]);
+void initialiseEchiquier(char grille[N][N]);
+void afficheEchiquier(const char grille[N][N], int num_coup, size_t temps_debut);
+
+// Gestion des couleurs, saisie d'un coup:
+int couleurAdverse(int couleur);
+int trouveCouleur(const char grille[N][N], int ligne, int colonne);
+bool estMajuscule(char c);
+bool convertitEnCoordonnees(const char notation[2], int coordonnees[2]);
+void saisieCoup(char grille[N][N]);
+
+// Implémentation du jeu d'échec:
+void trouvePositionRoi(const char grille[N][N], int positionRoi[2], int couleur);
+void realiseCoup(char grille[N][N], const int depart[2], const int arrivee[2]);
+bool realiseCoupSiValide(char grille[N][N], const int depart[2], const int arrivee[2]);
+bool estEnEchec(const char grille[N][N], int couleur);
+bool estCoupValide(const char grille[N][N], const int depart[2], const int arrivee[2]);
+bool estDeplacementValide(const char grille[N][N], const int depart[2], const int arrivee[2]);
+int partieTerminee(const char grille[N][N], int couleur);
+int listeDeplacementsValides(const char grille[N][N], const int depart[2], int deplacements[NB_MAX_DEPL][2]);
+
+
+int main(void)
+{
+	const size_t temps_debut = time(NULL); // temps de départ
+	// TODO
+	return 0;
+}
+
+//////////////////////////////////////////
+// Fonctions fournies:
+
+void effaceConsole(void)
+{
+	if (system("clear") != 0) {
+		printf("La console n'a pas pu etre effacee.\n");
+	}
+}
+
+// Si personne n'est en échec, passer pour couleur C_VIDE.
+void afficheCaseEnCouleur(char c, int couleurEnEchec)
+{
+	int couleurChar = estMajuscule(c) ? 2 : 4; // jaune ou mauve clair
+	if ((c == 'R' && couleurEnEchec == C_BLANC) || (c == 'r' && couleurEnEchec == C_NOIR)) { // roi en échec
+		couleurChar = 0; // rouge
+	}
+	if (c == CASE_VIDE) {
+		printf("%c", CASE_VIDE);
+	}
+	else {
+		printf("\e[1;%dm%c\e[0m", couleurChar+91, c);
+	}
+}
+
