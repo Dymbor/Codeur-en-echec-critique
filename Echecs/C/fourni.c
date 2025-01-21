ca729ddc2b45965d957309d626efb4f2682e3b69
Edwin659 <gwennaelle.airo-farulla@isen.yncrea.fr>
2025-01-21 16:08:16 +0100

    Update fourni.c
    convertir en coordonées

--- Echecs/C/fourni.c
+++ Echecs/C/fourni.c
@@ -189,4 +189,14 @@ void saisieCoup(char grille[N][N]){
  convertitEnCoordonnees(notation, coordonnees);
  realiseCoupSiValide(grille,depart, arrivee) ;
 
-};
+}
+
+bool convertitEnCoordonnees(const char notation[2], int coordonnees[2]){
+	if (notation[0] >= 'A' && notation[0] <= 'H')
+	{
+		coordonnees[1] = 65- (int)notation[0];
+	}
+	coordonnees[0] = 8- (int)notation[1];
+	
+	return estDansGrille(coordonnees[1], coordonnees[0]);
+}
